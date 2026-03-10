4bcf4f7152f953b427b7c55fc9c8ede058b4da2d
Mowbray1 <go38xiv@mytum.de>
2026-03-10 13:17:44 +0100

    initial setup

---
+++ src/main/java/com/skiCheck/demo/SkiCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.skiCheck.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skiCheck/demo/SkiCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skiCheck.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
